5c92949b3c16e7351b179034fa7b6ff6b6a1f247
Andi <andideve0@gmail.com>
2022-12-22 08:50:34 +0700

    refactor(use-typing): rename vars

--- src/hooks/use-typing.ts
+++ src/hooks/use-typing.ts
@@ -5,15 +5,15 @@ export default function useTyping(chars: string, _delayPerChar = 90) {
 
   const type = (char: string) => setResult((s) => s + char);
 
-  const typeWithTimeout = (char: string, boostTimeout?: boolean) => {
-    const delayPerChar = boostTimeout ? _delayPerChar - _delayPerChar * 0.4 : _delayPerChar;
+  const typeWithDelay = (char: string, boostDelay?: boolean) => {
+    const delayPerChar = boostDelay ? _delayPerChar - _delayPerChar * 0.4 : _delayPerChar;
     return setTimeout(() => type(char), delayPerChar);
   };
 
   useEffect(() => {
     if (result === chars) return;
     const leftChars = chars.substring(result.length);
-    const timeoutId = typeWithTimeout(leftChars[0], chars.indexOf(leftChars) < 8);
+    const timeoutId = typeWithDelay(leftChars[0], chars.indexOf(leftChars) < 8);
     return () => clearTimeout(timeoutId);
   }, [result, chars]);
 
